057d63d555d3ff14fac0205e08627d91ce2a6ce9
ManishS10 <104134547+ManishS10@users.noreply.github.com>
2022-05-02 18:55:13 +0530

    Add files via upload

---
+++ AngularDay4/Task2/app-fancy-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-fancy-card',
+  templateUrl: './app-fancy-card.component.html',
+  styleUrls: ['./app-fancy-card.component.css']
+})
+export class AppFancyCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
